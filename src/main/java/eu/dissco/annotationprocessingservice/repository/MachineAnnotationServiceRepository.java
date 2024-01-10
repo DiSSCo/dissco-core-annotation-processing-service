e0cc8697fb285b6caacf383b30ac924ba1be966e
southeo <soulaine.theo@gmail.com>
2024-01-10 14:44:04 +0100

    MasInput

--- src/main/java/eu/dissco/annotationprocessingservice/repository/MachineAnnotationServiceRepository.java
+++ src/main/java/eu/dissco/annotationprocessingservice/repository/MachineAnnotationServiceRepository.java
@@ -1,6 +1,7 @@
 package eu.dissco.annotationprocessingservice.repository;
 
 import static eu.dissco.annotationprocessingservice.database.jooq.Tables.MACHINE_ANNOTATION_SERVICES;
+import static eu.dissco.annotationprocessingservice.database.jooq.Tables.MAS_JOB_RECORD_NEW;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,22 +16,30 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class MachineAnnotationServiceRepository {
+
   private final DSLContext context;
   private final ObjectMapper mapper;
 
-  public Optional<MasInput> getMasInput(String masId){
+  public Optional<MasInput> getMasInput(String jobId) {
+    var query = context.select(MAS_JOB_RECORD_NEW.MAS_ID)
+        .from(MAS_JOB_RECORD_NEW)
+        .where(MAS_JOB_RECORD_NEW.JOB_ID.eq(jobId));
+
     return context.select(MACHINE_ANNOTATION_SERVICES.asterisk())
         .from(MACHINE_ANNOTATION_SERVICES)
-        .where(MACHINE_ANNOTATION_SERVICES.ID.eq(masId))
+        .where(MACHINE_ANNOTATION_SERVICES.ID.eq(query))
         .fetchSingle(this::mapToMasInput);
   }
 
   private Optional<MasInput> mapToMasInput(Record masRecord) {
     var masInput = masRecord.get(MACHINE_ANNOTATION_SERVICES.MAS_INPUT);
     try {
-      return masInput ==  null ? Optional.empty() : Optional.of(mapper.readValue(masInput.data(), MasInput.class));
-    } catch (JsonProcessingException e){
-      throw new DataBaseException("Unable to parse MasInput: " + masRecord.get(MACHINE_ANNOTATION_SERVICES.MAS_INPUT).data());
+      return masInput == null ? Optional.empty()
+          : Optional.of(mapper.readValue(masInput.data(), MasInput.class));
+    } catch (JsonProcessingException e) {
+      throw new DataBaseException(
+          "Unable to parse MasInput: " + masRecord.get(MACHINE_ANNOTATION_SERVICES.MAS_INPUT)
+              .data());
     }
 
   }
--- src/test/java/eu/dissco/annotationprocessingservice/repository/MachineAnnotationServiceRepositoryIT.java
+++ src/test/java/eu/dissco/annotationprocessingservice/repository/MachineAnnotationServiceRepositoryIT.java
@@ -2,15 +2,16 @@ package eu.dissco.annotationprocessingservice.repository;
 
 import static eu.dissco.annotationprocessingservice.TestUtils.CREATED;
 import static eu.dissco.annotationprocessingservice.TestUtils.ID;
+import static eu.dissco.annotationprocessingservice.TestUtils.ID_ALT;
+import static eu.dissco.annotationprocessingservice.TestUtils.JOB_ID;
 import static eu.dissco.annotationprocessingservice.TestUtils.MAPPER;
 import static eu.dissco.annotationprocessingservice.TestUtils.givenMasInput;
 import static eu.dissco.annotationprocessingservice.database.jooq.Tables.MACHINE_ANNOTATION_SERVICES;
+import static eu.dissco.annotationprocessingservice.database.jooq.Tables.MAS_JOB_RECORD_NEW;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import eu.dissco.annotationprocessingservice.database.jooq.tables.records.MachineAnnotationServicesRecord;
-import eu.dissco.annotationprocessingservice.domain.MasInput;
-import java.util.List;
+import eu.dissco.annotationprocessingservice.database.jooq.enums.MjrJobState;
 import org.jooq.JSONB;
 import org.jooq.exception.NoDataFoundException;
 import org.junit.jupiter.api.AfterEach;
@@ -29,6 +30,7 @@ class MachineAnnotationServiceRepositoryIT extends BaseRepositoryIT {
 
   @AfterEach
   void destroy() {
+    context.truncate(MAS_JOB_RECORD_NEW).execute();
     context.truncate(MACHINE_ANNOTATION_SERVICES).execute();
   }
 
@@ -38,7 +40,7 @@ class MachineAnnotationServiceRepositoryIT extends BaseRepositoryIT {
     populateDatabase(true);
 
     // When
-    var result = repository.getMasInput(ID);
+    var result = repository.getMasInput(JOB_ID);
 
     // Then
     assertThat(result).contains(givenMasInput());
@@ -50,7 +52,7 @@ class MachineAnnotationServiceRepositoryIT extends BaseRepositoryIT {
     populateDatabase(false);
 
     // When
-    var result = repository.getMasInput(ID);
+    var result = repository.getMasInput(JOB_ID);
 
     // Then
     assertThat(result).isEmpty();
@@ -59,7 +61,7 @@ class MachineAnnotationServiceRepositoryIT extends BaseRepositoryIT {
   @Test
   void testMachineAnnotationServiceMasDoesntExist() {
     // Then
-    assertThrows(NoDataFoundException.class, () -> repository.getMasInput(ID));
+    assertThrows(NoDataFoundException.class, () -> repository.getMasInput(JOB_ID));
   }
 
   void populateDatabase(boolean hasMasInput) throws Exception {
@@ -73,6 +75,14 @@ class MachineAnnotationServiceRepositoryIT extends BaseRepositoryIT {
         .set(MACHINE_ANNOTATION_SERVICES.CONTAINER_IMAGE, "IMG")
         .set(MACHINE_ANNOTATION_SERVICES.CONTAINER_IMAGE_TAG, "Tag")
         .set(MACHINE_ANNOTATION_SERVICES.MAS_INPUT, masInput)
+        .execute();
+
+    context.insertInto(MAS_JOB_RECORD_NEW)
+        .set(MAS_JOB_RECORD_NEW.JOB_ID, JOB_ID)
+        .set(MAS_JOB_RECORD_NEW.JOB_STATE, MjrJobState.SCHEDULED)
+        .set(MAS_JOB_RECORD_NEW.MAS_ID, ID)
+        .set(MAS_JOB_RECORD_NEW.TIME_STARTED, CREATED)
+        .set(MAS_JOB_RECORD_NEW.TARGET_ID, ID_ALT)
         .execute();
   }
 }
