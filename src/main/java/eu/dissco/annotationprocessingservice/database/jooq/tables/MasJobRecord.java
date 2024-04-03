bbdc43b36770cd220b95d01e3f556575ad4fecd8
southeo <soulaine.theo@gmail.com>
2024-04-03 11:10:22 +0200

    update pk name

--- src/main/java/eu/dissco/annotationprocessingservice/database/jooq/tables/MasJobRecord.java
+++ src/main/java/eu/dissco/annotationprocessingservice/database/jooq/tables/MasJobRecord.java
@@ -15,12 +15,12 @@ import java.util.function.Function;
 
 import org.jooq.Field;
 import org.jooq.ForeignKey;
-import org.jooq.Function10;
+import org.jooq.Function12;
 import org.jooq.JSONB;
 import org.jooq.Name;
 import org.jooq.Record;
 import org.jooq.Records;
-import org.jooq.Row10;
+import org.jooq.Row12;
 import org.jooq.Schema;
 import org.jooq.SelectField;
 import org.jooq.Table;
@@ -103,6 +103,16 @@ public class MasJobRecord extends TableImpl<MasJobRecordRecord> {
      */
     public final TableField<MasJobRecordRecord, Boolean> BATCHING_REQUESTED = createField(DSL.name("batching_requested"), SQLDataType.BOOLEAN, this, "");
 
+    /**
+     * The column <code>public.mas_job_record.error</code>.
+     */
+    public final TableField<MasJobRecordRecord, Object> ERROR = createField(DSL.name("error"), org.jooq.impl.DefaultDataType.getDefaultDataType("\"public\".\"error_code\""), this, "");
+
+    /**
+     * The column <code>public.mas_job_record.time_to_live</code>.
+     */
+    public final TableField<MasJobRecordRecord, Instant> TIME_TO_LIVE = createField(DSL.name("time_to_live"), SQLDataType.INSTANT, this, "");
+
     private MasJobRecord(Name alias, Table<MasJobRecordRecord> aliased) {
         this(alias, aliased, null);
     }
@@ -143,7 +153,7 @@ public class MasJobRecord extends TableImpl<MasJobRecordRecord> {
 
     @Override
     public UniqueKey<MasJobRecordRecord> getPrimaryKey() {
-        return Keys.MAS_JOB_RECORD_NEW_PK;
+        return Keys.MAS_JOB_RECORD_PK;
     }
 
     @Override
@@ -186,18 +196,18 @@ public class MasJobRecord extends TableImpl<MasJobRecordRecord> {
     }
 
     // -------------------------------------------------------------------------
-    // Row10 type methods
+    // Row12 type methods
     // -------------------------------------------------------------------------
 
     @Override
-    public Row10<String, MjrJobState, String, Instant, Instant, JSONB, String, String, MjrTargetType, Boolean> fieldsRow() {
-        return (Row10) super.fieldsRow();
+    public Row12<String, MjrJobState, String, Instant, Instant, JSONB, String, String, MjrTargetType, Boolean, Object, Instant> fieldsRow() {
+        return (Row12) super.fieldsRow();
     }
 
     /**
      * Convenience mapping calling {@link SelectField#convertFrom(Function)}.
      */
-    public <U> SelectField<U> mapping(Function10<? super String, ? super MjrJobState, ? super String, ? super Instant, ? super Instant, ? super JSONB, ? super String, ? super String, ? super MjrTargetType, ? super Boolean, ? extends U> from) {
+    public <U> SelectField<U> mapping(Function12<? super String, ? super MjrJobState, ? super String, ? super Instant, ? super Instant, ? super JSONB, ? super String, ? super String, ? super MjrTargetType, ? super Boolean, ? super Object, ? super Instant, ? extends U> from) {
         return convertFrom(Records.mapping(from));
     }
 
@@ -205,7 +215,7 @@ public class MasJobRecord extends TableImpl<MasJobRecordRecord> {
      * Convenience mapping calling {@link SelectField#convertFrom(Class,
      * Function)}.
      */
-    public <U> SelectField<U> mapping(Class<U> toType, Function10<? super String, ? super MjrJobState, ? super String, ? super Instant, ? super Instant, ? super JSONB, ? super String, ? super String, ? super MjrTargetType, ? super Boolean, ? extends U> from) {
+    public <U> SelectField<U> mapping(Class<U> toType, Function12<? super String, ? super MjrJobState, ? super String, ? super Instant, ? super Instant, ? super JSONB, ? super String, ? super String, ? super MjrTargetType, ? super Boolean, ? super Object, ? super Instant, ? extends U> from) {
         return convertFrom(toType, Records.mapping(from));
     }
 }
