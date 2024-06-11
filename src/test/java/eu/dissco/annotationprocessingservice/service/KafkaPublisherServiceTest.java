75d39d3c68c73bd226570cd1191fc57f8da0fd85
southeo <soulaine.theo@gmail.com>
2024-06-11 10:21:31 +0200

    coverage

--- src/test/java/eu/dissco/annotationprocessingservice/service/KafkaPublisherServiceTest.java
+++ src/test/java/eu/dissco/annotationprocessingservice/service/KafkaPublisherServiceTest.java
@@ -2,14 +2,18 @@ package eu.dissco.annotationprocessingservice.service;
 
 
 import static eu.dissco.annotationprocessingservice.TestUtils.MAPPER;
+import static eu.dissco.annotationprocessingservice.TestUtils.givenAnnotationEvent;
 import static eu.dissco.annotationprocessingservice.TestUtils.givenAnnotationProcessed;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.dissco.annotationprocessingservice.domain.annotation.Motivation;
 import eu.dissco.annotationprocessingservice.properties.KafkaConsumerProperties;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -55,4 +59,18 @@ class KafkaPublisherServiceTest {
     // Then
     then(kafkaTemplate).should().send(eq("createUpdateDeleteTopic"), anyString());
   }
+
+  @Test
+  void testPublishBatchAnnotation() throws JsonProcessingException {
+    // Given
+    given(consumerProperties.getTopic()).willReturn("topic");
+    var eventMessage = MAPPER.writeValueAsString(givenAnnotationEvent());
+
+    // When
+    service.publishBatchAnnotation(List.of(givenAnnotationEvent()));
+
+    // Then
+    then(kafkaTemplate).should(times(1)).send("topic", eventMessage);
+  }
+
 }
