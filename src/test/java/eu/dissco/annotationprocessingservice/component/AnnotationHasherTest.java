13bfd3be03e8ac789cf1424e958d7aca28c3f296
southeo <soulaine.theo@gmail.com>
2024-10-30 15:31:14 +0100

    roi -> ROI (#66)
    * update annotation fdo record
    
    * trivy
    
    * trivy

--- src/test/java/eu/dissco/annotationprocessingservice/component/AnnotationHasherTest.java
+++ src/test/java/eu/dissco/annotationprocessingservice/component/AnnotationHasherTest.java
@@ -45,7 +45,7 @@ class AnnotationHasherTest {
     map.put("ac:heightFrac", 0.1);
     var selector = new OaHasSelector()
         .withAdditionalProperty("@type", "oa:FragmentSelector")
-        .withAdditionalProperty("ac:hasRoi", map);
+        .withAdditionalProperty("ac:hasROI", map);
 
     var expected = UUID.fromString("bff30f04-e1ca-ed30-4841-d0138dfab477");
 
