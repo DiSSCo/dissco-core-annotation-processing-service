dd5ba37e0824d1070b3206242887bb073b462318
southeo <soulaine.theo@gmail.com>
2025-01-15 09:54:43 +0100

    sonar

--- src/main/java/eu/dissco/annotationprocessingservice/controller/AnnotationController.java
+++ src/main/java/eu/dissco/annotationprocessingservice/controller/AnnotationController.java
@@ -13,6 +13,12 @@ import eu.dissco.annotationprocessingservice.schema.Annotation;
 import eu.dissco.annotationprocessingservice.schema.AnnotationProcessingEvent;
 import eu.dissco.annotationprocessingservice.schema.AnnotationProcessingRequest;
 import eu.dissco.annotationprocessingservice.service.ProcessingWebService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,12 +37,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @Profile(Profiles.WEB)
 @RestController
-@RequestMapping("/")
+@RequestMapping("/annotation")
 @RequiredArgsConstructor
 public class AnnotationController {
 
   private final ProcessingWebService processingService;
+  protected static final String PREFIX_OAS = "Prefix of target ID";
+  protected static final String SUFFIX_OAS = "Suffix of target ID";
 
+  @Operation(summary = "Create a new annotation")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Annotation successfully created", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = Annotation.class))
+      })
+  })
   @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Annotation> createAnnotation(
       @RequestBody AnnotationProcessingRequest annotation)
@@ -46,6 +60,16 @@ public class AnnotationController {
     return ResponseEntity.ok(result);
   }
 
+  @Operation(summary = "Create a batch of annotations", description = """
+      Creates multiple annotations based on a single "parent" annotation and provided batch metadata.
+      All target objects that match the batch metadata's criteria will be annotated.
+      Returns original annotation as result.
+      """)
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Parent annotation successfully created. Child annotations successfully scheduled.", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = Annotation.class))
+      })
+  })
   @PostMapping(value = "batch", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Annotation> createAnnotationBatch(
       @RequestBody AnnotationProcessingEvent event)
@@ -56,10 +80,17 @@ public class AnnotationController {
     return ResponseEntity.ok(result);
   }
 
+  @Operation(summary = "Update an existing annotation")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Annotation successfully updated", content = {
+          @Content(mediaType = "application/json", schema = @Schema(implementation = Annotation.class))
+      })
+  })
   @PutMapping(value = "/{prefix}/{suffix}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Annotation> updateAnnotation(
-      @PathVariable("prefix") String prefix,
-      @PathVariable("suffix") String suffix, @RequestBody AnnotationProcessingRequest annotation)
+      @Parameter(description = PREFIX_OAS) @PathVariable("prefix") String prefix,
+      @Parameter(description = SUFFIX_OAS) @PathVariable("suffix") String suffix,
+      @RequestBody AnnotationProcessingRequest annotation)
       throws DataBaseException, FailedProcessingException, ConflictException, NotFoundException, AnnotationValidationException {
     checkId(prefix, suffix, annotation);
     log.info("Received hashedAnnotation update request for annotations {}",
@@ -68,9 +99,14 @@ public class AnnotationController {
     return ResponseEntity.ok(result);
   }
 
+  @Operation(summary = "Tombstone a given annotation")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "204", description = "Annotation successfully tombstoned")
+  })
   @DeleteMapping(value = "/{prefix}/{suffix}")
-  public ResponseEntity<Void> tombstoneAnnotation(@PathVariable("prefix") String prefix,
-      @PathVariable("suffix") String suffix,
+  public ResponseEntity<Void> tombstoneAnnotation(
+      @Parameter(description = PREFIX_OAS) @PathVariable("prefix") String prefix,
+      @Parameter(description = SUFFIX_OAS) @PathVariable("suffix") String suffix,
       @RequestBody AnnotationTombstoneWrapper annotationTombstoneWrapper)
       throws IOException, FailedProcessingException {
     var id = prefix + '/' + suffix;
---
+++ src/test/java/eu/dissco/annotationprocessingservice/controller/RestResponseEntityExceptionHandlerTest.java
@@ -0,0 +1,77 @@
+package eu.dissco.annotationprocessingservice.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import eu.dissco.annotationprocessingservice.exception.AnnotationValidationException;
+import eu.dissco.annotationprocessingservice.exception.ConflictException;
+import eu.dissco.annotationprocessingservice.exception.DataBaseException;
+import eu.dissco.annotationprocessingservice.exception.FailedProcessingException;
+import eu.dissco.annotationprocessingservice.exception.NotFoundException;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class RestResponseEntityExceptionHandlerTest {
+
+  private RestResponseEntityExceptionHandler exceptionHandler;
+
+  @BeforeEach
+  void setup() {
+    exceptionHandler = new RestResponseEntityExceptionHandler();
+  }
+
+  @Test
+  void testFailedProcessingException(){
+    // When
+    var result = exceptionHandler.handleException(new FailedProcessingException());
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  @Test
+  void testIOException(){
+    // When
+    var result = exceptionHandler.handleException(new IOException());
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  @Test
+  void testDataBaseException(){
+    // When
+    var result = exceptionHandler.handleException(new DataBaseException(""));
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
+  @Test
+  void testAnnotationValidationException(){
+    // When
+    var result = exceptionHandler.handleException(new AnnotationValidationException());
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+  }
+
+  @Test
+  void testConflictException(){
+    // When
+    var result = exceptionHandler.handleException(new ConflictException());
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+  }
+
+  @Test
+  void testNotFoundException(){
+    // When
+    var result = exceptionHandler.handleException(new NotFoundException("",""));
+
+    // Then
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  }
+}
