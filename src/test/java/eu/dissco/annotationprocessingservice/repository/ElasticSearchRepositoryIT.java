adf2b56cd58e3c3015a13cb5faa743beda898e0e
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-10-28 09:26:25 +0100

    Upgrade to elastic 9.2.0

--- src/test/java/eu/dissco/annotationprocessingservice/repository/ElasticSearchRepositoryIT.java
+++ src/test/java/eu/dissco/annotationprocessingservice/repository/ElasticSearchRepositoryIT.java
@@ -15,21 +15,19 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch.core.BulkRequest;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import com.fasterxml.jackson.databind.JsonNode;
 import eu.dissco.annotationprocessingservice.domain.AnnotationTargetType;
 import eu.dissco.annotationprocessingservice.properties.ElasticSearchProperties;
 import eu.dissco.annotationprocessingservice.schema.AnnotationBatchMetadata;
 import eu.dissco.annotationprocessingservice.schema.SearchParam;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.message.BasicHeader;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -43,7 +41,7 @@ import org.testcontainers.utility.DockerImageName;
 class ElasticSearchRepositoryIT {
 
   private static final DockerImageName ELASTIC_IMAGE = DockerImageName.parse(
-      "docker.elastic.co/elasticsearch/elasticsearch").withTag("8.6.1");
+      "docker.elastic.co/elasticsearch/elasticsearch").withTag("9.2.0");
   private static final String ANNOTATION_INDEX = "annotations";
   private static final String DIGITAL_SPECIMEN_INDEX = "digital-specimen";
   private static final String MEDIA_INDEX = "digital-media";
@@ -52,7 +50,7 @@ class ElasticSearchRepositoryIT {
   private static final ElasticsearchContainer container = new ElasticsearchContainer(
       ELASTIC_IMAGE).withPassword(ELASTICSEARCH_PASSWORD);
   private static ElasticsearchClient client;
-  private static RestClient restClient;
+  private static Rest5Client restClient;
   private ElasticSearchRepository repository;
 
   @BeforeAll
@@ -60,22 +58,15 @@ class ElasticSearchRepositoryIT {
     // Create the elasticsearch container.
     container.start();
 
-    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-    credentialsProvider.setCredentials(AuthScope.ANY,
-        new UsernamePasswordCredentials(ELASTICSEARCH_USERNAME, ELASTICSEARCH_PASSWORD));
+    var creds = Base64.getEncoder()
+        .encodeToString((ELASTICSEARCH_USERNAME + ":" + ELASTICSEARCH_PASSWORD).getBytes());
 
-    HttpHost host = new HttpHost("localhost",
-        container.getMappedPort(9200), "https");
-    final RestClientBuilder builder = RestClient.builder(host);
+    restClient = Rest5Client.builder(
+            new HttpHost("https", "localhost", container.getMappedPort(9200)))
+        .setDefaultHeaders(new Header[]{new BasicHeader("Authorization", "Basic " + creds)})
+        .setSSLContext(container.createSslContextFromCa()).build();
 
-    builder.setHttpClientConfigCallback(clientBuilder -> {
-      clientBuilder.setSSLContext(container.createSslContextFromCa());
-      clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
-      return clientBuilder;
-    });
-    restClient = builder.build();
-
-    ElasticsearchTransport transport = new RestClientTransport(restClient,
+    ElasticsearchTransport transport = new Rest5ClientTransport(restClient,
         new JacksonJsonpMapper(MAPPER));
 
     client = new ElasticsearchClient(transport);
